6f09b3eb497778be7b2f350d399149eb32c5577e
Nee-ayya <Kundan.mergu@gmail.com>
2024-05-20 20:56:34 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Homebar from './Components/homebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home.js';
+import Services from './pages/Services';
+import Products from './pages/Product';
+import SignUp from './pages/Signup';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Homebar />
+        <Routes>
+          <Route path="/" element={ <Home /> }/>
+          <Route path='/Services' element={ <Services /> } />
+          <Route path='/Product' element={ <Products /> } />
+          <Route path='/signup' element={ <SignUp /> } />
+        </Routes>
+      </Router>
+    </>
+
+  );
+}
+
+export default App;
