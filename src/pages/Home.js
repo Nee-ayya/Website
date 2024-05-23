05d67399297a8f8933029f017289d5fb85193f6b
Nee-ayya <Kundan.mergu@gmail.com>
2024-05-22 19:28:45 -0500

    Manual changes

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import Main from '../Components/Main';
+import Cards from '../Components/Cards';
+import Footer from '../Components/Footer';
+
+function Home() {
+    return (
+        <>
+            <Main />
+            <Cards />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
---
+++ src/pages/Signup.js
@@ -0,0 +1,7 @@
+"use client"
+import React from 'react'
+import '../App.css'
+
+export default function SignUp() {
+    return <h1 className='sign-up'>SIGN UP</h1>
+}
