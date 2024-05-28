57af58e36f8e2f70bea2717dcd764c5531369773
Nee-ayya <Kundan.mergu@gmail.com>
2024-05-27 20:16:48 -0500

    Fixed the mobile menu part of homepage

--- src/Components/homebar.js
+++ src/Components/homebar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './homebar.css';
 import { Button } from './button';
-import { useAuth } from './Authentication/AuthContext'; // Import useAuth hook
+import { useAuth } from './Authentication/AuthContext';
 
 function Homebar() {
     const [click, setClick] = useState(false);
@@ -12,14 +12,13 @@ function Homebar() {
     const closeMobileMenu = () => setClick(false);
     const { currentUser } = useAuth();
 
-    //function below doesnt show additional buttons if window is shrunk
     const showButton = () => {
         if (window.innerWidth <= 960) {
-            setButton(false)
+            setButton(false);
         } else {
-            setButton(true)
+            setButton(true);
         }
-    }
+    };
 
     useEffect(() => {
         showButton();
@@ -27,21 +26,19 @@ function Homebar() {
 
     window.addEventListener('resize', showButton);
 
-    // Helper function to extract the username from email
     const extractUsername = (email) => {
         if (email) {
-            const name = email.split('@')[0]
+            const name = email.split('@')[0];
             if (name.includes('.')) {
-                return name.split('.')[0]
-            } 
+                return name.split('.')[0];
+            }
             return name;
         } else {
             return '';
         }
     };
-    
 
-    return(
+    return (
         <nav className="Homebar">
             <div className="Homebar-container">
                 <Link to='/' className='homebar-logo'>
@@ -53,41 +50,34 @@ function Homebar() {
                 <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
                         <Link to='/' className='nav-links' onClick={ closeMobileMenu }>
-                            Home
+                            HOME
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='../Services' className='nav-links' onClick={ closeMobileMenu }>
-                            Games
+                        <Link to='/Games' className='nav-links' onClick={ closeMobileMenu }>
+                            GAMES
                         </Link>
                     </li>
                     <li className='nav-item'>
                         <Link to='/Product' className='nav-links' onClick={ closeMobileMenu }>
-                            Product
+                            PRODUCT
                         </Link>
                     </li>
-                </ul>
-                {currentUser ? (
-                        <div className='Homebar-container'>
-                            {/* <li className='nav-items'> */}
-                                <Link to='/profile' className='nav-links' onClick={ closeMobileMenu }>
-                                    Hello, { extractUsername(currentUser.email) }
-                                </Link>
-                            {/* </li> */}
-                        </div>
-                            
-                ) : (
-                    <div>
-                        {button && 
-                            (<Link to='login'>
+                    <li className='nav-item nav-item-right'>
+                        {currentUser ? (
+                            <Link to='/profile' className='nav-links'>
+                                Hello, { extractUsername(currentUser.email) }
+                            </Link>
+                        ) : (
+                            <Link to='/login' className='nav-links' onClick={ closeMobileMenu }>
                                 <Button buttonStyle='btn--outline'>SIGN UP / SIGN IN</Button>
-                            </Link>)
-                        }
-                        </div>
-                    )}
+                            </Link>
+                        )}
+                    </li>
+                </ul>
             </div>
         </nav>
-    )
+    );
 }
 
-export default Homebar
+export default Homebar;
